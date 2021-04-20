1af6c0b28c9002eff19be2974a404e4eac51c339
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-20 23:42:19 +0530

    [Prajakta]: Calculated Wages for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,10 @@
-//UC3
+//UC4
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck)
 {
@@ -19,7 +20,12 @@ function getWorkingHours(empCheck)
     }
 }
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+
+for (let day = 0; day< NUM_OF_WORKING_DAYS; day++)
+{
+    let empCheck =Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);    
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+console.log("Total Hours: "+ empHrs +"   Emp Wage: "+empWage);
