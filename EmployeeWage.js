107c11c1245860c7622d7a53b5c369f112dba84e
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-20 23:49:22 +0530

    [Prajakta]: Calculated wages till number off working days or total working hours per month is reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,13 @@
-//UC4
+//UC5
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
+const NUM_OF_WORKING_DAYS = 10;
+const MAX_HOURS_IN_MONTH = 100
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
 function getWorkingHours(empCheck)
 {
@@ -19,13 +22,11 @@ function getWorkingHours(empCheck)
     
     }
 }
-let empHrs = 0;
-
-for (let day = 0; day< NUM_OF_WORKING_DAYS; day++)
+while(totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
-    let empCheck =Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);    
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.round() * 10) % 3;
+       totalEmpHrs += getWorkingHours(empCheck);
 }
-
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hours: "+ empHrs +"   Emp Wage: "+empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays + " Total Hrs:"+  totalEmpHrs + "   Emp Wage: " + empWage);
